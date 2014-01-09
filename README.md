d631bcb1d8711dc39ec69b5711dfcbf38d8765ac
tassadarliu <rhapsodyn@gmail.com>
2014-01-09 20:54:44 +0800

    versionizer almost ready

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 rhapsodyn's tool set
 
+## Install
+
+`npm install -g rtools`
+
+## Versionize
+
+Give every static resources (js/css) in your html file a version.
+
+See `rtools versionize --help`
+
 ## License
 Copyright (c) 2014 rhapsodyn  
 Licensed under the MIT license.
--- test/versionizer_test.js
+++ test/versionizer_test.js
@@ -23,9 +23,40 @@ var versionizer = require('../lib/versionizer.js');
 */
 
 var emptyHtml = "<html><head><title></title></head><body></body></html>";
+var scriptAndCssWithOutVersion = '<script type="text/javascript" src="a.js"></script>' +
+	'<link rel="stylesheet" type="text/css" href="b.css"/>';
+var scriptHasVersion = '<script type="text/javascript" src="c.js?v=1389259582261"></script>';
 
-exports.testConvertWithNoSrc = function(test) {
+
+exports.testConvert_WithNoSrc = function(test) {
 	var sameResult = versionizer.convert(emptyHtml);
 	test.deepEqual(sameResult, emptyHtml);
-  test.done();
-}
+	test.done();
+};
+
+exports.testConvert_WithBothScriptAndCss_WithOutVersion = function(test) {
+	var result = versionizer.convert(scriptAndCssWithOutVersion);
+	var src = result.substring(result.indexOf('src="') + 5, result.indexOf('">'));
+	var href = result.substring(result.indexOf('href="') + 6, result.length - 2);
+
+	test.notDeepEqual(src.indexOf("?v="), -1);
+	test.notDeepEqual(href.indexOf("?v="), -1);
+
+	var versionLength = Date.now().toString().length;
+	var srcVersion = src.substring(src.indexOf("=") + 1);
+	var hrefVersion = href.substring(href.indexOf("=") + 1);
+
+	test.deepEqual(srcVersion.length, versionLength);
+	test.deepEqual(hrefVersion.length, versionLength);
+	test.done();
+};
+
+exports.testConvert_WithVersionAlready = function(test) {
+	var result = versionizer.convert(scriptHasVersion);
+	var version = "1389259582261";
+	var newVersion = result.substring(result.indexOf("c.js?v=") + 7, result.indexOf('"></script>'));
+
+	test.deepEqual(newVersion.length, version.length);
+	test.notDeepEqual(newVersion, version);
+	test.done();
+};
